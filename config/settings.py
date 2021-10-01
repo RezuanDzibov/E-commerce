f81ac4c8ca8768f858d809f987fd639c4115a552
RezuanDzibov <rezuan.dzbov@gmail.com>
2021-10-01 17:08:14 +0300

    Ready part 3

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    # 'config.middlewares.Except'
 ]
 
 
